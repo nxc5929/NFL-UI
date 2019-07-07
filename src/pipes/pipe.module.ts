8c476212ff9d16869b170de29c1fddb8d967beff
Nathan Connor <nathan_connor@ultimatesoftware.com>
2019-07-07 18:41:04 -0400

    Added things

--- src/pipes/pipe.module.ts
+++ src/pipes/pipe.module.ts
@@ -1,10 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { HelperPipe } from 'src/debug/HelperPipe';
+import { HelperPipe } from 'src/pipes/helper.pipe';
 import { WinnerPipe } from './check-winner.pipe';
+import { SubmittedPicksPipe } from './submitted-picks.pipe';
 
 @NgModule({
-  declarations: [HelperPipe, WinnerPipe],
+  declarations: [HelperPipe, WinnerPipe, SubmittedPicksPipe],
   imports: [HttpClientModule],
   providers: []
 })
