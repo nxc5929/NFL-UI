fe9c14dd16e12441eca01122acef71f60a5acd7d
Nathan <nxc5929@rit.edu>
2019-09-03 18:59:53 -0400

    Https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: '/api/v1',
-  secureApiURL: '/api/v1/secure',
-  adminApiURL: '/api/v1/admin',
+  apiURL: 'https://nfl-picks-connor.herokuapp.com/api/v1',
+  secureApiURL: 'https://nfl-picks-connor.herokuapp.com/api/v1/secure',
+  adminApiURL: 'https://nfl-picks-connor.herokuapp.com/api/v1/admin',
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,9 +11,9 @@
 
 export const environment = {
   production: true,
-  apiURL: 'http://nfl-picks-connor.herokuapp.com/api/v1',
-  secureApiURL: 'http://nfl-picks-connor.herokuapp.com/api/v1/secure',
-  adminApiURL: 'http://nfl-picks-connor.herokuapp.com/api/v1/admin',
+  apiURL: 'https://nfl-picks-connor.herokuapp.com/api/v1',
+  secureApiURL: 'https://nfl-picks-connor.herokuapp.com/api/v1/secure',
+  adminApiURL: 'https://nfl-picks-connor.herokuapp.com/api/v1/admin',
 };
 
 /*
